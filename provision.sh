ca67addd91b9c139a8648b104fc02d1aefcb91ee
Omar <44298100+omards8@users.noreply.github.com>
2020-10-28 12:08:28 +0100

    Actualizamos las carpetas que se sincronizan con Vagrant

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 apt-get -y update
 apt-get -y install nginx
+cp -avr /usr/share/nginx/www/ /vagrant/www
 rm -rf /etc/nginx/sites-enabled
 cp -R /vagrant/sites-enabled/ /etc/nginx/sites-enabled/
-rm -f "usr/share/nginx/www"
+rm -f usr/share/nginx/www
